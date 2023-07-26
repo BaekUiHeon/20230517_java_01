d85b5067e86ad17e6a49a17424a885595ccdf893
BaekUiHeon <qordmlgjs1@gmail.com>
2023-07-26 17:26:25 +0900

    페이징,데이터베이스 insert
    페이징,데이터베이스 insert
    날짜부분을 String으로 만들어줘서 처리함.

--- 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/controller/StudentGetController.java
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/controller/StudentGetController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kh.test.jdbckh.student.model.dao.StudentDao;
+import kh.test.jdbckh.student.model.service.StudentService;
 import kh.test.jdbckh.student.model.vo.StudentVo;
 
 /**
@@ -21,7 +22,6 @@ public class StudentGetController extends HttpServlet {
      */
     public StudentGetController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -30,11 +30,13 @@ public class StudentGetController extends HttpServlet {
 		// 1. 전달받은 parameter 읽어내기
 		String studentNo = request.getParameter("sno");
 		System.out.println(studentNo);
-		// 전달받은 데이터를 활용해 DB학생 상세 정보 가져오기
-		StudentDao dao =new StudentDao();
-		StudentVo vo = dao.selectOneStudent(studentNo);
+		// 2. 전달받은 데이터를 활용해 DB학생 상세 정보 가져오기
+		StudentService service =new StudentService();
+		StudentVo vo = service.selectOneStudent(studentNo);
+		// 3. DB로부터 전달받은 데이터를 JSP에 전달함.
 		request.setAttribute("svo", vo);
-		request.getRequestDispatcher("/WEB-INF/view/student/get.jsp").forward(request, response);;
+		// 4. JSP 파일 forward로 열기
+		request.getRequestDispatcher("/WEB-INF/view/student/get.jsp").forward(request, response);
 	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
