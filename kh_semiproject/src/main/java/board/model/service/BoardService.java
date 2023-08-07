49ab899e6cf0d8633c79eda1fa8f3c50e814b02d
BaekUiHeon <135290607+BaekUiHeon@users.noreply.github.com>
2023-08-08 00:03:27 +0900

    세미프로젝트 추가구현
    댓글삭제구현, 대댓글 html구현중.

--- kh_semiproject/src/main/java/board/model/service/BoardService.java
+++ kh_semiproject/src/main/java/board/model/service/BoardService.java
@@ -17,11 +17,11 @@ public class BoardService {
 	private static Connection conn=null;
 
 	
-	public int deleteComment(String idx,String cidx) {
+	public int deleteComment(String cidx) {
 		int result=-1;
 		conn=getConnection();
 		BoardDao dao=new BoardDao();
-		result=dao.deleteComment(conn,idx,cidx);
+		result=dao.deleteComment(conn,cidx);
 		close(conn);
 		return result;
 	}
